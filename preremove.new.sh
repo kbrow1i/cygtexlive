d8e01c8fa797cfadaf6315b9f3aa77fdb4b2a5e4
Ken Brown <kbrown@cornell.edu>
2014-07-01 16:23:44 -0400

    Fix postinstall/preremove scripts to deal properly with Kanji maps

---
+++ preremove.new.sh
@@ -0,0 +1,14 @@
+PN="texlive-collection-langjapanese"
+dir="/etc/preremove"
+
+eval $(/usr/bin/updmap-sys --showoption kanjiEmbed | grep '^kanjiEmbed=' \
+	    | sed -e 's/\(^kanjiEmbed=[^ ]*\).*/\1/')
+eval $(/usr/bin/updmap-sys --showoption kanjiVariant | grep '^kanjiVariant=' \
+	    | sed -e 's/\(^kanjiVariant=[^ ]*\).*/\1/')
+echo '#!/bin/bash' > ${dir}/${PN}.out
+/usr/bin/sed -e "s/@kanjiEmbed@/${kanjiEmbed}/" \
+	     -e "s/@kanjiVariant@/${kanjiVariant}/" \
+	     < ${dir}/${PN}.in \
+	     >> ${dir}/${PN}.out
+chmod 755 ${dir}/${PN}.out
+exec ${dir}/${PN}.out
