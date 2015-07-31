d9883d3cfb46e468125a22b2bc4619c884919fd3
Ken Brown <kbrown@cornell.edu>
2015-07-31 13:30:36 -0400

    Update for TL2015

---
+++ fix_requires.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+eval $(grep '^NAME=' *.cygport)
+eval $(grep '^VERSION=' *.cygport)
+eval $(grep '^RELEASE=' *.cygport)
+
+if grep -q '^ARCH=noarch$' *.cygport
+then
+    ARCH=noarch
+else
+    ARCH=$(uname -m)
+fi
+
+D=${NAME}-${VERSION}-${RELEASE}.${ARCH}
+sed -i -e '/^requires:/s/python3//' ${D}/dist/${NAME}/setup.hint
