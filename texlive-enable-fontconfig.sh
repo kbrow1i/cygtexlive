8df927bdcc1b4799c44e08984830a4c989f95c4d
Ken Brown <kbrown@cornell.edu>
2015-09-11 11:21:33 -0400

    Use /usr/share/fontconfig/conf.avail instead of /etc/fonts/conf.avail
    Also bump release number to 2.

--- texlive-enable-fontconfig.sh
+++ texlive-enable-fontconfig.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-ln -s /etc/fonts/conf.avail/09-texlive.conf /etc/fonts/conf.d/09-texlive.conf
+ln -sf /usr/share/fontconfig/conf.avail/09-texlive.conf /etc/fonts/conf.d/09-texlive.conf
 /usr/bin/fc-cache -f \
   /usr/share/texmf-dist/fonts/opentype \
   /usr/share/texmf-dist/fonts/truetype \
