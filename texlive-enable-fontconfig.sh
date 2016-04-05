4d913d129bf6869e68f4d42259393c58a903c14d
Ken Brown <kbrown@cornell.edu>
2016-04-05 16:03:28 -0400

    Remove dependence on fontconfig

--- texlive-enable-fontconfig.sh
+++ texlive-enable-fontconfig.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 ln -sf /usr/share/fontconfig/conf.avail/09-texlive.conf /etc/fonts/conf.d/09-texlive.conf
-/usr/bin/fc-cache -f \
+/usr/libexec/fc-cache-1 -f \
   /usr/share/texmf-dist/fonts/opentype \
   /usr/share/texmf-dist/fonts/truetype \
   /usr/share/texmf-dist/fonts/type1
