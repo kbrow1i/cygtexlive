ee10a9745d0391f51073bbd8785e7b231743d360
Ken Brown <kbrown@cornell.edu>
2017-02-18 13:09:33 -0500

    Remove mathextra and science; merged into mathscience (and -doc)

--- texlive-collection-mathextra/fix_requires.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-eval $(grep '^NAME=' *.cygport)
-eval $(grep '^VERSION=' *.cygport)
-eval $(grep '^RELEASE=' *.cygport)
-
-ARCH=noarch
-D=${NAME}-${VERSION}-${RELEASE}.${ARCH}
-sed -i -e '/^requires:/s/python3//' ${D}/dist/${NAME}/setup.hint
