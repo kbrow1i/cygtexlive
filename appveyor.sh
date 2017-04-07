43301705e03f58e54a3709316ac003835537e1b4
Ken Brown <kbrown@cornell.edu>
2017-04-07 18:22:22 -0400

    Only try to fetch recently changed collections

--- appveyor.sh
+++ appveyor.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 exit_status=0
 
-./fetch.sh || exit_status=1
+./fetch.sh 2>&1 | tee myfetch.log || exit_status=1
 
-tar -cJf artifact.tar.xz failures.txt \
-    texlive-collection-*/texlive-collection-*/log
+tar -cJf artifact.tar.xz failures.txt myfetch.log
 
 exit $exit_status
--- fetch.sh
+++ fetch.sh
@@ -8,10 +8,14 @@ for d in $dirs
 do
     echo "Entering $d ..."
     cd $d
-    if ! cygport ${d}.cygport fetch
+    eval $(grep VERSION ${d}.cygport)
+    if [ ${VERSION} = 20170407 ]
     then
-	echo "Fetching ${d} failed" >> ../failures.txt
-	exit_status=1
+	if ! cygport ${d}.cygport fetch
+	then
+	    echo "Fetching ${d} failed" >> ../failures.txt
+	    exit_status=1
+	fi
     fi
     echo "Leaving $d"
     cd ..
