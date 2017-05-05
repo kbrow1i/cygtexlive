82d70f64554693c7f7ae0c15b2bd36c1dd9c0bfd
Ken Brown <kbrown@cornell.edu>
2017-05-04 22:23:43 -0400

    Remove obsolete fix_requires.sh files

--- texlive-collection-langarabic/fix_requires.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-eval $(grep '^NAME=' *.cygport)
-eval $(grep '^VERSION=' *.cygport)
-eval $(grep '^RELEASE=' *.cygport)
-
-PVR=${NAME}-${VERSION}-${RELEASE}
-D=${PVR}.noarch
-sed -i -e '/^requires:/s/ python3//' ${D}/dist/${NAME}/${PVR}.hint
