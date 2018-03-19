a40375c25697e4e285baf13e36429f351c7e2413
Ken Brown <kbrown@cornell.edu>
2018-03-19 18:13:41 -0400

    Update collections as of 20180319

--- 01-update_release.sh
+++ 01-update_release.sh
@@ -6,11 +6,11 @@ for d in $dirs
 do
     echo "Entering $d ..."
     cd $d
-    sed -i -e 's/^VERSION=.*/VERSION=20170520/' ${d}.cygport
+    sed -i -e 's/^VERSION=.*/VERSION=20180319/' ${d}.cygport
     rm -rf ${d}-20170524-1.noarch
-    # sed -i -e 's/^RELEASE=.*/RELEASE=1/' ${d}.cygport
-    # mkdir -p save
-    # mv -f *.tar.xz save/
+    sed -i -e 's/^RELEASE=.*/RELEASE=1/' ${d}.cygport
+    mkdir -p save
+    mv -f *.tar.xz save/
     echo "Leaving $d"
     cd ..
 done
